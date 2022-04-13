42873c745c022726151d7ff899ba10642bf772ac
ChristofferBillman <christoffer.billman@gmail.com>
2022-04-13 15:38:31 +0200

    Added so that you can download all subscribers as XML.

---
+++ AdvertSystem/AdvertSystem/Controllers/XmlController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdvertSystem.Controllers
+{
+	public class XmlController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		[HttpGet]
+		public IActionResult Get()
+		{
+			return View();
+		}
+	}
+}
